e8294ea2085da2e471f899f1a374220849796127
Graham Gilbert <graham.gilbert@airbnb.com>
2017-02-06 15:41:08 +0000

    We'll just let the OS load it at reboot

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,5 @@ class post_upgrade_run (
   String $puppet_command = '/opt/puppetlabs/bin/puppet agent -t'
 ) {
   class {'post_upgrade_run::install': } ->
-  class {'post_upgrade_run::service': } ->
   Class['post_upgrade_run']
 }
--- manifests/service.pp
+++
@@ -1,7 +0,0 @@
-# Set up the service for the good stuff
-class post_upgrade_run::service {
-  service {'org.macadmins.post_upgrade_run':
-    ensure => 'running',
-    enable => true
-  }
-}
